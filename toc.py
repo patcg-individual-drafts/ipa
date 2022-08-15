36db402953880795c0b0eaf9feb095c0ca60ab7e
Martin Thomson <mt@lowentropy.net>
2022-08-15 16:34:15 +1000

    Iteration on script
    Get the `<sub>` thing cleaned up.
    Use argparse to set the ToC depth.

--- toc.py
+++ toc.py
@@ -1,45 +1,56 @@
 #!/usr/bin/env python3
 
-from fileinput import FileInput
+import argparse
+from fileinput import input
+import re
 from sys import argv, stderr
 import tempfile
 
-if len(argv) < 2:
-    stderr.write(f"Usage: {sys.argv[0]} <md>")
-    exit(2)
 
 _seen = dict()
+_xml = re.compile(r"</?[a-zA-Z]\w+>")
 
 
 def href(title):
     """
     An attempt to replicate how GitHub creates title anchors.
     """
+    global _xml
+    title = _xml.sub("", title)
+    h = "".join(
+        [x.lower() if x.isalnum() else ("-" if x == " " else "") for x in title]
+    )
+
     global _seen
-    h = "".join([x.lower() if x.isalnum() else ("-" if x == " " else "") for x in title])
     _seen[h] = _seen.get(h, 0) + 1
     if _seen[h] > 1:
         return f"{h}-{_seen[h]-1}"
     return h
 
+
+parser = argparse.ArgumentParser(description="Add a markdown ToC")
+parser.add_argument("-d", "--depth", dest="depth", type=ord, default=3, help="Maximum heading level to include")
+parser.add_argument("file", nargs=1, help="Markdown file")
+args = parser.parse_args()
+
 tocfile = tempfile.NamedTemporaryFile()
 
 with open(tocfile.name, "w") as toc:
     intoc = False
-    for line in FileInput(files=(argv[1])):
+    for line in input(files=(args.file)):
         if line.startswith("#"):
             line = line.rstrip("#").strip()
             title = line.lstrip("#")
             level = len(line) - len(title)
             title = title.strip()
-            toc.write(f"{'  ' * (level - 1)}* [{title}](#{href(title)})\n")
+            if level <= args.depth:
+                toc.write(f"{'  ' * (level - 1)}* [{title}](#{href(title)})\n")
         elif intoc:
             continue
 
 
-
 intoc = False
-for line in FileInput(files=(argv[1])):
+for line in input(files=(args.file)):
     if line.startswith("#"):
         title = line.strip("#").strip().lower()
         intoc = title == "table of contents"
